c2f8ed5deb0fcb67d1b615cd15e0c00dc906c79e
MarcoMendez7 <Marco.men7@outlook.com>
2021-03-11 16:52:11 -0700

    Practica1

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u1_practica1_layoutsymaslayouts/Main3Activity.kt
@@ -0,0 +1,14 @@
+package mx.tecnm.tepic.ladm_u1_practica1_layoutsymaslayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class Main3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
